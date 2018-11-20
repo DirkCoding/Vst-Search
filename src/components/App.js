fce97ff1379a61e0147cb7bdd58fbe078a1ce77e
Kim-Dirk Linsenmeier <KimCoding@web.de>
2018-11-20 11:47:56 +0100

    add dropdown listed alphabetically_2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+// import styled from 'styled-components'
+// import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import DropdownInstruments from './DropdownInstruments'
 /*import Home from './Home'
 import Config from './Config'*/
 
-const Wrapper = styled.section`
+/*const Wrapper = styled.section`
   display: grid;
   grid-template-rows: auto 50px;
   height: 100vh;
@@ -32,7 +32,7 @@ const Wrapper = styled.section`
       color: white;
     }
   }
-`
+`*/
 
 export default class App extends Component {
   render() {
