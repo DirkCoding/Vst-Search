c71d7c89ac57efb408b61b8f44ac94ad9dbde55a
Kim-Dirk Linsenmeier <KimCoding@web.de>
2018-11-21 14:40:15 +0100

    Added basic styling

--- src/components/Createcard.js
+++ src/components/Createcard.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 
 const Card = styled.section`
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: 40px 40px;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
   align-items: center;
+  justify-content: center;
   margin: 25px auto;
   padding: 25px;
   background: white;
@@ -13,33 +14,39 @@ const Card = styled.section`
   height: auto;
   border-radius: 8px;
   box-shadow: 0 8px 16px rgba(180, 180, 180, 0.4);
+  border: 1px solid #000;
 `
+
 const Title = styled.h1`
   font-size: 23px;
-  grid-column: 1 / 8;
+  grid-column: 2 / 3;
 `
 const Company = styled.h2`
   font-size: 18px;
-  grid-column: 1/6;
+  grid-column: 2 / 3;
 `
 const Paragraph = styled.p`
   font-size: 12px;
-  grid-column: 1/4;
+  grid-column: 2 / 3;
 `
 
 const Hyperlink = styled.a`
   font-size: 12px;
-  grid-column: 1/4;
+  grid-column: 2 / 3;
 `
 const Image = styled.img`
-  position: relative;
-  float: left;
-  width: 100px;
-  height: 100px;
+  width: 150px;
+  height: 150px;
   background-position: 50% 50%;
-  background-repeat: no-repeat;
   background-size: cover;
+  grid-column: 1 / 2;
+  grid-row: 1 / 4;
+  margin: 20px;
 `
+
+const Libraryheader = styled.section``
+const Librarytext = styled.section``
+const Information = styled.section``
 
 class Createcard extends Component {
   render() {
@@ -55,13 +62,19 @@ class Createcard extends Component {
 
     return (
       <Card>
-        <Title>Library: {title}</Title>
-        <Company>Company: {company}</Company>
+        <Information>
+          <Libraryheader>
+            <Title>{title}</Title>
+            <Company>{company}</Company>
+          </Libraryheader>
+          <Librarytext>
+            <Paragraph>Instruments included: {sections}</Paragraph>
+            <Paragraph>Ensemble Patch available: {ensemblepatch}</Paragraph>
+            <Paragraph>Price: {price}</Paragraph>
+            <Hyperlink>Link to manufacturer: {url}</Hyperlink>
+          </Librarytext>
+        </Information>
         <Image src={image} />
-        <Hyperlink>Link to manufacturer: {url}</Hyperlink>
-        <Paragraph>Ensemble Patch available: {ensemblepatch}</Paragraph>
-        <Paragraph>Price: {price}</Paragraph>
-        <Paragraph>Instruments included: {sections}</Paragraph>
       </Card>
     )
   }
