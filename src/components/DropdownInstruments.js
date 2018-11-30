3e92bc5cc6fa88d8e77aabfbb0128ba61cc38e3c
Kim-Dirk Linsenmeier <KimCoding@web.de>
2018-11-30 16:24:19 +0100

    Add styled dropdown

--- src/components/DropdownInstruments.js
+++ src/components/DropdownInstruments.js
@@ -5,6 +5,33 @@ import libraryData from '../resources/library-data.json'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
+const SelectWrapper = styled.section`
+  font-family: fontello;
+  content: '\f107';
+  font-size: 20px;
+
+  right: 15px;
+  top: 10px;
+  color: #fff;
+`
+
+const Select = styled.select`
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  /* height: 25px; */
+  padding: 5px;
+  color: black;
+  border: 1px solid;
+  border-color: rgb(184, 134, 11);
+  font-size: 12px;
+  box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.3);
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+`
+
 class DropdownInstruments extends Component {
   static propTypes = {
     searchChange: PropTypes.func.isRequired
@@ -12,8 +39,8 @@ class DropdownInstruments extends Component {
   render() {
     const { searchChange } = this.props
     return (
-      <div>
-        <select onChange={searchChange} placeholder="Choose an instrument">
+      <SelectWrapper>
+        <Select onChange={searchChange} placeholder="Choose an instrument">
           <option default>Select Instrument</option>
           {// get list of instruments
           libraryData
@@ -31,8 +58,8 @@ class DropdownInstruments extends Component {
             .map(instrument => (
               <option key={instrument}>{instrument}</option>
             ))}
-        </select>
-      </div>
+        </Select>
+      </SelectWrapper>
     )
   }
 }
